1dd32bc81725e5a702ceb6342f84080bf88e720c
Zexuz <Robin.Edbom@gmail.com>
2017-03-02 15:17:16 +0100

    Interface for login

--- csharp/ROS/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ROS
-{
-    public class Class1
-    {
-    }
-}
---
+++ csharp/ROS/Interfaces/DatabaseConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ROS.Interfaces
+{
+    public class DatabaseConnection : IDatabaseConnection
+    {
+        public string Host { get; set; }
+        public string Database { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+
+        public SqlConnection SqlConnection { get; set; }
+
+
+        public void Login()
+
+        {
+            SqlConnection = new SqlConnection("");
+            return SqlConnection;
+        }
+
+        void Test()
+        {
+            using (Login())
+            {
+
+            }
+        }
+    }
+}
