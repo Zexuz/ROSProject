3c137b4d2aa506b919943e56231393c5162c60ca
Robin Edbom <robin.edbom@gmail.com>
2017-03-14 12:02:51 +0100

    Added models

---
+++ csharp/ROS/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace ROS.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string  ZipCode { get; set; }
+
+        public string  City { get; set; }
+
+        public string StreetAdress  { get; set; }
+
+        public string BoxNumber { get; set; }
+
+        public string  Contry { get; set; }
+    }
+}
