41f3d8e8d43aa43fccfc81f90c6eebeeb4377530
Niclas Valfridsson <waffe91@gmail.com>
2017-03-14 09:57:42 +0100

    Added MVC and Folder Structure

---
+++ csharp/RoS.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoS.MVC.Startup))]
+namespace RoS.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
