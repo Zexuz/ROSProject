80fd7d5d915e36280ce45549623732fdb08f1f37
Linus Gustafsson <osnusmeforfasensatan@hotmail.com>
2017-03-28 09:54:43 +0200

    added boat

---
+++ csharp/ROS.Domain/ROS.Domain/Services/BoatService.cs
@@ -0,0 +1,26 @@
+﻿using ROS.Domain.Models;
+using ROS.Domain.PocoClasses.Entries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace ROS.Domain.Services
+{
+    public class BoatService
+    {
+        private EntityDataModel db = new EntityDataModel();
+        private MapperConfiguration config = new MapperConfiguration(cfg => {cfg.CreateMap<PocoBoat, Boat>();
+        });
+
+        public void Add(PocoBoat boat)
+        {
+            var mapper = config.CreateMapper();
+            Boat newBoat = mapper.Map<Boat>(boat);
+            db.Boats.Add(newBoat);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ csharp/ROS.Domain/ROS.MVC/Controllers/BoatController.cs
@@ -0,0 +1,88 @@
+﻿using ROS.Domain.Services;
+using ROS.Domain.PocoClasses.Entries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ROS.MVC.Controllers
+{
+    public class BoatController : Controller
+    {
+        private BoatService _boatService = new BoatService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "SailNumber,Name,Type,Handicap,Description")] PocoBoat newBoat)
+        {
+            try
+            {
+                _boatService.Add(newBoat);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Boat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Boat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Boat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Boat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
