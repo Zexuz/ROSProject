1de67af4bb9d7f00661552de621cc0f6c276e4ce
Zexuz <Robin.Edbom@gmail.com>
2017-03-29 11:11:12 +0200

    Fixed test for GetAll users

---
+++ csharp/ROS.Domain/ROS.Domain/Contexts/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using ROS.Domain.Models;
+
+namespace ROS.Domain.Contexts
+{
+    public class UserContext:DbContext
+    {
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
--- csharp/ROS.Domain/ROS.Test/UserControllerTests.cs
+++ csharp/ROS.Domain/ROS.Test/UserControllerTests.cs
@@ -1,4 +1,11 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using FakeItEasy;
+using NUnit.Framework;
+using ROS.Domain.Contexts;
 using ROS.Domain.Models;
 
 
@@ -11,8 +18,58 @@ namespace ROS.Test
         [Test]
         public void User_Sign_Up_Succeded_Test()
         {
-            var user = new User();
+            var data = new List<User>
+            {
+                new User()
+                {
+                    AddressContactId = 1,
+                    Email = "Test@gmail.com",
+                    FirstName = "Robin",
+                    LastName = "Edbom",
+                    Password = "pw",
+                    DateOfBirth = DateTime.Parse("1996-11-07"),
+                    Id = 1
+                }
+            }.AsQueryable();
+
+            // Arrange
+            var fakeDbSet = A.Fake<DbSet<User>>(o => o.Implements(typeof(IQueryable<User>)).Implements(typeof(IDbAsyncEnumerable<User>)));
+
+
+            A.CallTo(() => ((IQueryable<User>)fakeDbSet).Provider).Returns(data.Provider);
+            A.CallTo(() => ((IQueryable<User>)fakeDbSet).Expression).Returns(data.Expression);
+            A.CallTo(() => ((IQueryable<User>)fakeDbSet).ElementType).Returns(data.ElementType);
+            A.CallTo(() => ((IQueryable<User>)fakeDbSet).GetEnumerator()).Returns(data.GetEnumerator());
+
+            var fakeContext = A.Fake<UserContext>();
+            A.CallTo(() => fakeContext.Users).Returns(fakeDbSet);
+
+            var userService = new UserService(fakeContext);
+
+            // Act
+            var users = userService.GetAll().ToList();
+
+            // Assert
+            Assert.AreEqual(1, users.Count(), "Count should be 1");
+            Assert.AreEqual(1, users.First().Id, "Id Should be 1");
+            Assert.AreEqual("Robin", users.First().FirstName, "FirstName Should be Robin");
+            Assert.AreEqual("Edbom", users.First().LastName, "LastName Should be Edbom");
         }
 
+    }
+
+    public class UserService
+    {
+        private readonly UserContext _userContext;
+
+        public UserService(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _userContext.Users;
+        }
     }
 }
