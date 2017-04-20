690326876a0addd16a884c7d6fba86362b6b40bf
Zexuz <Robin.Edbom@gmail.com>
2017-04-20 14:56:58 +0200

    Fixed Kenans shit

---
+++ csharp/ROS.Domain/ROS.MVC/PocoClasses/Entries/JoinEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ROS.MVC.PocoClasses.Entries
+{
+    public class JoinEntry
+    {
+
+        [DisplayName("Entry number, this is a nr that is 6 char long")]
+        public string EntryNumber { get; set; }
+    }
+}
