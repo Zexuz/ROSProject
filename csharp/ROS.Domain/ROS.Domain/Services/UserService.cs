848ec17ebc7b39dea14f7327f1018d36401bd525
Zexuz <Robin.Edbom@gmail.com>
2017-04-26 09:47:59 +0200

    UserContext fixed

--- csharp/ROS.Domain/ROS.Domain/Services/UserService.cs
+++ csharp/ROS.Domain/ROS.Domain/Services/UserService.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using ROS.Domain.Contexts;
+using ROS.Domain.Interfaces.ContextInterfaces;
 using ROS.Domain.Models;
 
 namespace ROS.Domain.Services
 {
     public class UserService
     {
-        private readonly UserContext _userContext;
+        private readonly IRosContext<User> _userContext;
 
-        public UserService(UserContext userContext)
+        public UserService(IRosContext<User> userContext)
         {
             _userContext = userContext;
         }
 
         public IEnumerable<User> GetAll()
         {
-            return _userContext.Users;
+            return _userContext.DbSet;
         }
 
         public User Add(User user)
@@ -26,8 +27,8 @@ namespace ROS.Domain.Services
             if (!isUserValid(user))
                 throw new ArgumentException();
 
-            var returnedUser = _userContext.Users.Add(user);
-            _userContext.SaveChanges();
+            var returnedUser = _userContext.DbSet.Add(user);
+            _userContext.Context.SaveChanges();
             return returnedUser;
         }
 
@@ -40,14 +41,14 @@ namespace ROS.Domain.Services
 
         public User Remove(User user)
         {
-            var removedUser = _userContext.Users.Remove(user);
-            _userContext.SaveChanges();
+            var removedUser = _userContext.DbSet.Remove(user);
+            _userContext.Context.SaveChanges();
             return removedUser;
         }
 
         public User Edit(User user)
         {
-            var dbUser = _userContext.Users.SingleOrDefault(u => u.Id == user.Id);
+            var dbUser = _userContext.DbSet.SingleOrDefault(u => u.Id == user.Id);
 
             if (dbUser == null)
             {
@@ -60,7 +61,7 @@ namespace ROS.Domain.Services
             dbUser.FirstName = user.FirstName;
             dbUser.LastName = user.LastName;
             dbUser.Password = user.Password;
-            _userContext.SaveChanges();
+            _userContext.Context.SaveChanges();
 
             return user;
         }
